941b9b46e6eb2bed45576c069c56c4c2710eda35
moussa <m.sow@deal.fr>
2020-10-13 08:56:04 +0200

    crud operations

---
+++ src/com/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,58 @@
+package com.hibernate.demo;
+import com.hibernate.demo.entity.Student;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student createdStudent1 = new Student("Cheikh", "fall", "fall@luv2code.com");
+			Student createdStudent2 = new Student("Serigne", "thioune", "paul@luv2code.com");
+			session.beginTransaction();
+			session.save(createdStudent1);
+			session.save(createdStudent2);
+			session.getTransaction().commit();
+			System.out.println("Creation done!");
+			
+			System.out.println("deleting student1 first way........");
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			Student gettedStudent= session.get(Student.class, createdStudent1.getId());
+			session.delete(gettedStudent);
+			session.getTransaction().commit();
+			System.out.println("deleted student" + gettedStudent);
+			
+
+			System.out.println("deleting all students second way........");
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			session.createQuery("delete from Student")
+			.executeUpdate();
+			session.getTransaction().commit();
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
